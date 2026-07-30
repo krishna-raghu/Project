cc4b3fc8ead73f7023e8b421fa7141d4404ef708
Anshika Kansal <anshikakansal001@gmail.com>
2026-07-30 16:41:35 +0530

    Delete project directory

--- project/demo/src/main/java/com/example/demo/ServiceNeoRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.demo;
-
-import org.springframework.data.neo4j.repository.Neo4jRepository;
-
-public interface ServiceNeoRepository extends Neo4jRepository<ServiceNode, String> {
-}
